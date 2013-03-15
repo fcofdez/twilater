aa78bcf98eb7557171d1007dc9571939756c1ccd
Francisco Fernández Castaño <fr.fernandez@flowlab.biz>
2013-03-15 17:51:45 +0100

    Rails app Skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twilater::Application.config.secret_key_base = '4d445e99e60c7fc45ed22b5c88c5b3afa076397ca5f6697cb7e7fcccee76f8cd10db8d85dffa1bcfb47241466448abe06d3898e51c79c280331160c981cd522b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Twilater::Application.config.session_store :encrypted_cookie_store, key: '_twilater_session'
