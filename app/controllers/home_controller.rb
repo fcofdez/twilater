98dc11dbc9f6b35a322d8fca5a2d23b3d0691ab0
Francisco Fernández Castaño <fr.fernandez@flowlab.biz>
2013-03-16 04:43:31 +0100

    Getting favorite tweets

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
 
+  def welcome
+    redirect_to home_path if user_signed_in?
+  end
+
   def index
+    @tweets = current_user.favorite_tweets
   end
 end
