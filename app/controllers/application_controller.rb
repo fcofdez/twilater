4d744a0296909af25e290079e051f4e6d9d4b226
Francisco Fernández Castaño <fr.fernandez@flowlab.biz>
2013-03-16 03:33:02 +0100

    Adding helper methods related to sessions, like current_user. Also adding notice and flash messages front end.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,27 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+  helper_method :user_signed_in?
+  helper_method :authenticate_user!
+
+  private
+
+  def current_user
+    begin
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue ActiveRecord::RecordNotFound
+      nil
+    end
+  end
+
+  def user_signed_in?
+    current_user.present?
+  end
+
+  # Checks if user is logged
+  def authenticate_user!
+    redirect_to root_url, :alert => 'You need to sign in for access to this page.' if !current_user
+  end
 end
