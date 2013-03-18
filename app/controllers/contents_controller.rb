39606a297656564e892e10644ce310a5e5434c44
Francisco Fernández Castaño <fercas@gmail.com>
2013-03-18 12:21:24 +0100

    Loading via ajax contents to a modal window

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -3,7 +3,7 @@ class ContentsController < ApplicationController
   respond_to :html, :js
 
   def index
-    @contents = Content.text_search(params[:query]).where(user: current_user).order('created_at DESC').page(1).per(5)
+    @contents = Content.text_search(params[:query]).select(:title, :read, :id).where(user: current_user).order('created_at DESC').page(1).per(5)
   end
 
   def show
