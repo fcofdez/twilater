b8d5031d21c5e6974c4a1d7d712b9ffe2498c0f5
Francisco Fernández Castaño <fr.fernandez@flowlab.biz>
2013-03-20 11:54:46 +0100

    Misspell on migration

--- db/migrate/20130319195347_delete_user_fields.rb
+++ db/migrate/20130319195347_delete_user_fields.rb
@@ -10,8 +10,9 @@ class DeleteUserFields < ActiveRecord::Migration
                                   token: user.token,
                                   secret: user.secret,
                                   user_id: user.id)
+
       # Temporaly change values of identity to user_id before rename it
-      Contents.where(user_id: user.id).update_all user_id: identity.id
+      Content.where(user_id: user.id).update_all user_id: identity.id
     end
 
     rename_column :contents, :user_id, :identity_id
